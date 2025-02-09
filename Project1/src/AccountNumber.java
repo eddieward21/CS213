fe0738a3abe461d18b14e1946da3174c5498bdc1
Eddie Ward <eddie.musashi1@gmail.com>
2025-02-09 14:01:54 -0500

    implemented the classes AccountNumber, Date, AccountType. tests for Branch class.

--- Project1/src/AccountNumber.java
+++ Project1/src/AccountNumber.java
@@ -2,39 +2,59 @@ import java.util.Objects;
 import java.util.Random;
 
 public class AccountNumber implements Comparable<AccountNumber> {
-    private static final int SEED = 9999;
+    private static final int SEED = 9999;  // Fixed: Constant should not be modified
     private Branch branch;
     private AccountType type;
     private String number;
 
-    public AccountNumber(int SEED, Branch branch, AccountType type, String number) {
-        this.SEED = SEED;
+    // Constructor
+    public AccountNumber(Branch branch, AccountType type, String number) {
         this.branch = branch;
         this.type = type;
         this.number = number;
     }
 
+    // Generate a 4-digit random number based on a seed
     public int generateRandomNumber(int seed) {
-        Random random = new Random(SEED);
-        int fourDigitNumber = 1000 + random.NextInt(9000);
+        Random random = new Random(seed);
+        return 1000 + random.nextInt(9000);  // Fixed: Corrected nextInt()
     }
 
+    // Override equals() for object comparison
     @Override
     public boolean equals(Object obj) {
-
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        AccountNumber that = (AccountNumber) obj;
+        return Objects.equals(branch, that.branch) &&
+                Objects.equals(type, that.type) &&
+                Objects.equals(number, that.number);
     }
 
+    // Override toString() for a readable format
     @Override
     public String toString() {
-
+        return "AccountNumber{" +
+                "branch=" + branch +
+                ", type=" + type +
+                ", number='" + number + '\'' +
+                '}';
     }
 
+    // Implement compareTo() for sorting
     @Override
-    public String compareTo() {
-
+    public int compareTo(AccountNumber other) {
+        return this.number.compareTo(other.number);
     }
 
     public static void main(String[] args) {
-        System.out.println(generateRandomNumber(9999));
+        AccountNumber account = new AccountNumber(new Branch(), new AccountType(), "123456");
+
+        // Call generateRandomNumber correctly
+        int randomNum = account.generateRandomNumber(SEED);
+        System.out.println("Generated Account Number: " + randomNum);
+
+        // Print account details
+        System.out.println(account);
     }
 }
